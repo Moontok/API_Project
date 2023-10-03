1019bf9c789d3ab858c661a7e1d6787d0a593487
Zackary <zspink@nea.k12.ar.us>
2023-10-03 15:03:55 -0500

    Base Work

---
+++ API/socialapi/routers/post.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, HTTPException
+
+from socialapi.models.post import Comment, CommentIn, UserPost, UserPostIn
+
+router = APIRouter()
+post_table = {}
+comment_table = {}
+
+
+def find_post(post_id: int):
+    print("here")
+    return post_table.get(post_id)
+
+
+@router.post("/post", response_model=UserPost, status_code=201)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@router.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
+
+
+@router.post("/comment", response_model=Comment, status_code=201)
+async def create_comment(comment: CommentIn):
+    print("yo")
+    post = find_post(comment.post_id)
+    print("yo1")
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    data = comment.model_dump()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
+
+
