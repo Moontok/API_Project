0b0ce2c86ea2f73c020a3fa9cd6882e01a2e82d3
Zackary <zspink@nea.k12.ar.us>
2023-10-06 15:43:41 -0500

    updates

--- API/socialapi/main.py
+++ API/socialapi/main.py
@@ -2,7 +2,9 @@ import logging
 
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from asgi_correlation_id import CorrelationIdMiddleware
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import http_exception_handler
 
 from socialapi.database import database
 from socialapi.logging_conf import configure_logging
@@ -19,6 +21,12 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
+app.add_middleware(CorrelationIdMiddleware, header_name="X-Correlation-ID")
 
 app.include_router(post_router)
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handle_logging(request, exc):
+    logger.error(f"HTTPException: {exc.status_code} {exc.detail}")
+    return await http_exception_handler(request, exc)
