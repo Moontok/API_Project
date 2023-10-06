91a5676d377286bc7e08bac8c520070feedab3ad
Zackary <zspink@nea.k12.ar.us>
2023-10-06 11:41:00 -0500

    Adding Loggers

--- API/socialapi/main.py
+++ API/socialapi/main.py
@@ -1,12 +1,18 @@
+import logging
+
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from socialapi.database import database
+from socialapi.logging_conf import configure_logging
 from socialapi.routers.post import router as post_router
 
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
     await database.connect()
     yield
     await database.disconnect()
